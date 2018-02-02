c330f720a21ba01d6808561ee3bf75cdc93308a9
moodyssbm <moodyssbm@gmail.com>
2018-02-02 01:30:20 -0500

    redirect back to home page after server update

--- form-submit.php
+++ form-submit.php
@@ -1,4 +1,6 @@
 <?php
     $data = $_POST['name'] . "," . $_POST['email'] . "," . $_POST['format'] . "\n";
     $ret = file_put_contents('./data/list.csv', $data, FILE_APPEND | LOCK_EX);
+    header("Location: http://tapes.immoody.com/"); /* Redirect browser */
+    exit();
 ?>

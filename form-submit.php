d72143ff5350794eb990d2905975ce415b203c09
moodyssbm <moodyssbm@gmail.com>
2018-02-18 19:46:14 -0500

    started from scratch

--- form-submit.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $data = $_POST['name'] . "," . $_POST['email'] . "," . $_POST['format'] . "\n";
-    $ret = file_put_contents('./data/list.csv', $data, FILE_APPEND | LOCK_EX);
-    header("Location: http://tapes.immoody.com/"); /* Redirect browser */
-    exit();
-?>
